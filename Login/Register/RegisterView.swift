4d7bf14157941f8052f856a1a55e1cdb7dda58a1
João Pedro Volponi <jpvrosada@hotmail.com>
2024-09-12 18:52:20 -0300

    Setup RegisterView and RegisterViewController

--- Login/Register/RegisterView.swift
+++ Login/Register/RegisterView.swift
@@ -9,5 +9,15 @@ import Foundation
 import UIKit
 
 class RegisterView: UIView {
+    private let viewController: RegisterViewController
     
+    init(viewController: RegisterViewController) {
+        self.viewController = viewController
+        super.init(frame: .zero)
+        backgroundColor = .red
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
--- Login/Register/RegisterViewController.swift
+++ Login/Register/RegisterViewController.swift
@@ -9,5 +9,21 @@ import Foundation
 import UIKit
 
 class RegisterViewController: UIViewController {
-
+    
+    let viewModel: RegisterViewModelProtocol
+    var registerView: RegisterView?
+    
+    init(viewModel: RegisterViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func loadView() {
+        registerView = RegisterView(viewController: self)
+        self.view = self.registerView ?? UIView()
+    }
 }
