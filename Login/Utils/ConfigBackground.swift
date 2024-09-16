cfde59bfe1091f69f771ca77da1666b7f9569463
João Pedro Volponi <jpvrosada@hotmail.com>
2024-09-15 21:25:44 -0300

    Extension ConfigBackground

--- Login/Utils/ConfigBackground.swift
+++ Login/Utils/ConfigBackground.swift
@@ -14,8 +14,8 @@ class ConfigBackground {
         let gradientLayer = CAGradientLayer()
         
         gradientLayer.colors = [
-            UIColor(named: "PurplePrimary"),
-            UIColor(named: "PurpleSecondary")
+            UIColor(hex: "#52396D").cgColor,
+            UIColor(hex: "#3a215d").cgColor
         ]
 
         gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
@@ -23,5 +23,23 @@ class ConfigBackground {
         gradientLayer.frame = view.bounds
 
         view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+}
+
+// MARK: Config hexadecimal
+extension UIColor {
+    convenience init(hex: String) {
+        var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
+        hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
+
+        var rgb: UInt64 = 0
+        Scanner(string: hexSanitized).scanHexInt64(&rgb)
+
+        let red = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
+        let green = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
+        let blue = CGFloat(rgb & 0x0000FF) / 255.0
+
+        self.init(red: red, green: green, blue: blue, alpha: 1.0)
     }
 }
