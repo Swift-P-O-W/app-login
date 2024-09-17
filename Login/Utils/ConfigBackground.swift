f8b5dac55c63df31c3fa8c75c838514f6ce92e1e
João Pedro Volponi <jpvrosada@hotmail.com>
2024-09-16 22:22:44 -0300

    Refactor configBackground

--- Login/Utils/ConfigBackground.swift
+++ Login/Utils/ConfigBackground.swift
@@ -10,36 +10,36 @@ import UIKit
 
 class ConfigBackground {
     
-    func applyGradientBackground(view: UIView) {
+    private var gradientLayer: CAGradientLayer?
+    
+    func applyGradientBackground(view: UIView, firstColor: String, secondColor: String) {
         let gradientLayer = CAGradientLayer()
         
         gradientLayer.colors = [
-            UIColor(hex: "#52396D").cgColor,
-            UIColor(hex: "#3a215d").cgColor
+            UIColor(hex: firstColor).cgColor,
+            UIColor(hex: secondColor).cgColor
         ]
-
         gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
         gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
-        gradientLayer.frame = view.bounds
-
+        
         view.layer.insertSublayer(gradientLayer, at: 0)
+        
+        self.gradientLayer = gradientLayer
     }
-    
 }
 
-// MARK: Config hexadecimal
 extension UIColor {
     convenience init(hex: String) {
         var hexSanitized = hex.trimmingCharacters(in: .whitespacesAndNewlines)
         hexSanitized = hexSanitized.replacingOccurrences(of: "#", with: "")
-
+        
         var rgb: UInt64 = 0
         Scanner(string: hexSanitized).scanHexInt64(&rgb)
-
+        
         let red = CGFloat((rgb & 0xFF0000) >> 16) / 255.0
         let green = CGFloat((rgb & 0x00FF00) >> 8) / 255.0
         let blue = CGFloat(rgb & 0x0000FF) / 255.0
-
+        
         self.init(red: red, green: green, blue: blue, alpha: 1.0)
     }
 }
