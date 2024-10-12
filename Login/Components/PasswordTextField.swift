242a6e0ab021a0d24b21dec22cc37b0bdccb6968
Luiz Gabriel Rebelatto <lbrebelatto@gmail.com>
2024-10-12 18:00:45 -0300

    feat: connect firebase and add spacements left side

--- Login/Components/PasswordTextField.swift
+++ Login/Components/PasswordTextField.swift
@@ -29,6 +29,10 @@ class PasswordTextField: UIView {
         textField.backgroundColor = UIColor(named: "PurplePrimary")
         textField.textColor = .white
         textField.rightViewMode = .always
+        
+        let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: 12, height: textField.frame.height))
+        textField.leftView = paddingView
+        textField.leftViewMode = .always
         return textField
     }()
     
--- Login/Screens/Login/LoginViewModel.swift
+++ Login/Screens/Login/LoginViewModel.swift
@@ -7,15 +7,31 @@
 
 import Foundation
 import UIKit
+import Firebase
 
 protocol LoginViewModelProtocol {
     var coordinator: LoginMainCoordinator { get }
+    func signInUser(email: String, password: String)
 }
 
 class LoginViewModel: LoginViewModelProtocol {
+    
     var coordinator: LoginMainCoordinator
+    var auth: Auth = Auth.auth()
     
     init(coordinator: LoginMainCoordinator) {
         self.coordinator = coordinator
     }
+    
+    func signInUser(email: String, password: String) {
+        auth.signIn(withEmail: email, password: password) { authResult, error in
+            if error == nil {
+                print("Sucesso Login")
+                self.coordinator.callHome()
+            } else {
+                print("Error login, error: \(error?.localizedDescription ?? "")")
+            }
+        }
+    }
+    
 }
